94c93cfa502a2428aa9e8d664a2529fa91072dab
johnblakey <blakeyjo@oregonstate.edu>
2017-12-17 21:06:22 -0700

    Update text output of test server

--- webserverTest/server.js
+++ webserverTest/server.js
@@ -1,12 +1,12 @@
 /**
  * Created by jb on 12/9/17.
- * Simple hello world server.
+ * Simple web server.
  */
 
 var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello sdcommander!\n');
+    res.end('Welcome to the future rivel level website location. Lose the uncertainty, not the gear.\n');
 }).listen(2000, "0.0.0.0");
 console.log('Server running at http://riverlevel:2000/');
