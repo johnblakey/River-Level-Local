3c17861c6004377afbe63ba259f9bbd1bc2b93a5
johnblakey <blakeyjo@oregonstate.edu>
2017-12-17 18:04:01 -0700

    External ip attempt

--- api/server.js
+++ api/server.js
@@ -13,7 +13,7 @@ let app = express();
 app.use(bodyParser.json());
 
 /** open sqlite database from local file */
-let dbSource = "/home/jb/Development/Production/River-Level-Database/dbRiverLevel/levels.db";
+let dbSource = "/home/js/Development/Production/River-Level-Database/dbRiverLevel/levels.db";
 let db = new sqlite3.Database(dbSource, sqlite3.OPEN_READONLY, (err) => {
     if (err) {
         console.error(err.message);
@@ -22,8 +22,7 @@ let db = new sqlite3.Database(dbSource, sqlite3.OPEN_READONLY, (err) => {
 
     /* initialize the express app */
     let port = "3000";
-    let address = "24.56.179.191";
-    const server = app.listen(port, address, () => {
+    const server = app.listen(port, () => {
         var {address, port} = server.address();
         console.log(`Express app running at http://${address}:${port}`);
     });
