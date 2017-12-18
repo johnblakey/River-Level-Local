8d82656bed52db11b2b8fbdfd29a4d8b86eb05ba
johnblakey <blakeyjo@oregonstate.edu>
2017-12-17 21:20:51 -0700

    Fixed typo in output

--- webserverTest/server.js
+++ webserverTest/server.js
@@ -7,6 +7,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Welcome to the future rivel level website location. Lose the uncertainty, not the gear.\n');
+    res.end('Welcome to the future river level website location. Lose the uncertainty, not the gear.\n');
 }).listen(2000, "0.0.0.0");
 console.log('Server running at http://riverlevel:2000/');
